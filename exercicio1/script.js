22cb797f356fc7dd3911956842bcc90796afd571
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2025-05-09 09:41:24 -0300

    Add files via upload

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,1 +1,9 @@
-console.log("hello word!")
+function somar () {
+    let n1 = 0;
+    let n2 = 0;
+
+    n1 = Number(window.prompt('Digite um número:'));
+    n2 = Number(window.prompt('Digite outro número:'));
+    let soma = n1 + n2 
+    alert(soma);
+}
