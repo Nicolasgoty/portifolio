789249e3a8b098357fc28b0b2ec01df7beee1a50
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2025-05-19 08:22:05 -0300

    Add files via upload

--- exercicio 3/script2.js
+++ exercicio 3/script2.js
@@ -6,32 +6,65 @@ function sobra(){
     n1 = Number(window.prompt("Digite o 1° número"));
     n2 = Number(window.prompt("Digite o 2° número"));
     res = n1 % n2;
-    alert("O resultado é " + res + ".") ; 
-    //essa função é a da divisãoo com sobra, ele divide os 2 números e mostra o resultado e a sobra
+    alert("O resultado é " + res + ".") ;
+     if (res == 0){
+        alert("este número é impar")
+     }
+     else{
+        alert("Este número é par")
+     } 
+    //essa função é a da divisão com sobra, ele divide os 2 números e mostra o resultado e a sobra
 }
 function multiplicacao(){ 
     n1 = Number(window.prompt("Digite o 1° número"));
     n2 = Number(window.prompt("Digite o 2° número"));
     res = n1 * n2;
     alert("O resultado é " + res + ".") ; 
+     if (res  == 0){
+        alert("este número é par")
+     }
+     else{
+        alert("Este número é impar" )
+     }
     //essa função é a da multiplicação, ele multiplica os 2 números e mostra o resultado
 }
 function soma(){
- n1 = Number(window.prompt('Digite um número:'));
+    n1 = Number(window.prompt('Digite um número:'));
     n2 = Number(window.prompt('Digite outro número:'));
     res = n1 + n2;
     alert("O resultado é " + res + ".");
+     if (res == 0){
+        alert("este número é par")
+     }
+     else{
+        alert("Este número é impar" )
+     }
     //essa função é a da soma, ele soma os 2 números e mostra o resultado
 }
 function subtracao(){
- n1 = Number(window.prompt('Digite um número:'));
+    n1 = Number(window.prompt('Digite um número:'));
     n2 = Number(window.prompt('Digite outro número:'));
     res = n1 - n2;
     alert("O resultado é " + res + ".");
+     if (res  == 0){
+        alert("este número é par")
+     }
+     else{
+        alert("Este número é impar" )
+     }
+    //essa função é a da subtração, ele subtrai os 2 números e mostra o resultado
 }
 function divisao(){
- n1 = Number(window.prompt('Digite um número:'));
+    n1 = Number(window.prompt('Digite um número:'));
     n2 = Number(window.prompt('Digite outro número:'));
     res = n1 / n2;
     alert("O resultado é " + res + ".");
+     //essa função é a da divisão0, ele divide os 2 números e mostra o resultado 
+
+     if (res  == 0){
+        alert("este número é impar")
+     }
+     else{
+        alert("Este número é par")
+     }
 }
