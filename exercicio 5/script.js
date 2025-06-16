065a77c936d13a2eb411a9f7714b7f31ac76d003
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2025-06-16 08:51:11 -0300

    Add files via upload

--- exercicio 5/script.js
+++ exercicio 5/script.js
@@ -1,23 +1,24 @@
-    let ale = parseInt(Math.random() * 101);
-    let comp = document.querySelector('#resultado');
-    let jogador;
-    // declarei 3 variáveis
-function pensar(){;
- jogador = Number(window.prompt("digite um número de 1 a 100"));
- console.log(ale);
-    // faz uma pergunta para o usuário digitar um número de 1 a 100 
- if( jogador != ale){
+let aleatorio = parseInt(Math.random() * 101)
+let mostraresultado = document.querySelector('#resultado');
+let jogador;
+let tent = 0;
 
-    comp.innerHTML = `<p> Você errou, continue tentando` 
-    if( jogador > ale){
-        comp.innerHTML = `<p> Você errou, continue tentando, mas tente um número MENOR!`
-    }
-    else{
-        comp.innerHTML = `<p> Você errou, continue tentando, mas tente um número MAIOR!!`
-    }
-     }else{
-    comp.innerHTML = `<p> Parabéns, você conseguiu!! Foram somente {tentativas}`
-    }
-    // mostra se o usuário acertou, se não acertou, pede para continuar
+function pensar() {
+    jogador = Number(window.prompt("Digite um número entre 1 a 100"))
+    
+    if(jogador > 0 && jogador <101) {
 
+        tent += 1;
+    
+       if (jogador != aleatorio) {
+        if(jogador > aleatorio) {
+                mostraresultado.innerHTML +=  `<p><b> TENTATIVA ${tent}:</b> Você tentou o número <b>${jogador}</b>, tente um NUMERO <b>MENOR</b>`;
+        } else {
+                mostraresultado.innerHTML +=  `<p><b> TENTATIVA ${tent}:</b>Voce tentou o número <b>${jogador}</b>, tente um NUMERO <b>MAIOR</b>`;
+        }
+       } else {
+        mostraresultado.innerHTML += `<p><b>Parabéns você acertou ${aleatorio} em ${tent} tentativas</b></p>`;
+       }
+         }else { alert("Por favor, digite um número entre 1 e 100.");
+}
 }
---
+++ exercicio 8/scrpt.js
@@ -0,0 +1,13 @@
+let n1;
+let n2;
+let n3 = document.querySelector('#resultado');
+
+function verificarestacao() {
+     n1 = Number(window.prompt("Digite seu nome"));
+     n2 =  Number(window.prompt("Digite seu mês de nascimento"));
+     if(n2 = dezembro, janeiro, fevereiro) {
+      
+     }
+     
+}
+ 
