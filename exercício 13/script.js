0ab7e4acb7aca3fa3831417a7efb91a164f95557
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2025-07-07 12:00:41 -0300

    Add files via upload

--- exercício 13/script.js
+++ exercício 13/script.js
@@ -1,7 +1,18 @@
-
 function somar(){
-    let soma = 0;
-    let n1;
-    let n2;
-   
-}
+
+        let numeros = [];
+        while (true) {
+            let numero = prompt("Digite um número (0 para parar):");
+            if (numero === "0") break;
+            numeros.push(Number(numero));
+        }
+        
+        if (numeros.length > 0) {
+            let soma = numeros.reduce((a, b) => a + b, 0);
+            let media = soma / numeros.length;
+            document.getElementById('resultado').innerHTML = `A soma destes números é: ${soma} <br> A média destes números é: ${media}`;
+        } else {
+            document.getElementById('resultado').innerHTML = "Nenhum número foi digitado.";
+        }
+
+        }
